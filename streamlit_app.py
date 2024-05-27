0140d948be4866a2554cf7eadc53f32d03ee80c3
Jonathan Little <jonathan.little@drivetime.com>
2024-05-27 14:26:39 -0700

    CHANGING DATA CHART

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import requests
+
+
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
 st.write(
@@ -33,7 +36,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -49,7 +53,3 @@ if ingredients_list:
 
             st.success('Your Smoothie is ordered!',icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
